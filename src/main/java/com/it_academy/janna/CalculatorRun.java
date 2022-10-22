eaea483f0877d44e5d7ff9dd417a49e90d2fd5e8
Janna Marchanka <81391091+jannamartek@users.noreply.github.com>
2022-10-22 22:42:51 +0300

    Update CalculatorRun.java

--- src/main/java/com/it_academy/janna/CalculatorRun.java
+++ src/main/java/com/it_academy/janna/CalculatorRun.java
@@ -3,7 +3,6 @@ package com.it_academy.janna;
 
 public class CalculatorRun {
     public static void main(String[] args) {
-
         CalculatorUserActions.printWelcomeMessage();
         CalculatorUserActions.getUserInput();
         CalculatorUserActions.getUserChoice();
